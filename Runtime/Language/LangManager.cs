6451edcac4da4d398616a62dec289bdcb10a54ea
craftwill <william-gin1@hotmail.com>
2025-05-12 10:22:18 -0400

    Remove dependency to newtonsoft .net lib

--- Runtime/Language/LangManager.cs
+++ Runtime/Language/LangManager.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using Newtonsoft.Json;
-
 namespace Bytes.Language
 {
     /// <summary>
@@ -118,10 +116,8 @@ namespace Bytes.Language
             try
             {
                 TextAsset targetFile = Resources.Load<TextAsset>(filePath);
-                //Dictionary<string, string> langData = JsonUtility.FromJson<Dictionary<string, string>>(targetFile.text);
 
-                // Newtonsoft json serializer needed
-                var langData = JsonConvert.DeserializeObject<Dictionary<string, string>>(targetFile.text);
+                var langData = SimpleJsonParser.ParseDictionary(targetFile.text);
 
                 if (additive) { GetInstance().currentLangDataInstances[index] = langData; }
                 else { GetInstance().currentLangDataInstances[0] = langData; }
@@ -141,5 +137,39 @@ namespace Bytes.Language
             return GetInstance() != null && GetInstance().currentLangDataInstances.Length > 0;
         }
 
+    }
+}
+
+public static class SimpleJsonParser
+{
+    public static Dictionary<string, string> ParseDictionary(string json)
+    {
+        var result = new Dictionary<string, string>();
+
+        // Remove outer braces
+        json = json.Trim().TrimStart('{').TrimEnd('}');
+
+        // Split into key-value pairs
+        var pairs = json.Split(',');
+
+        foreach (var pair in pairs)
+        {
+            var split = pair.Split(new[] { ':' }, 2);
+            if (split.Length != 2) continue;
+
+            string key = Unquote(split[0].Trim());
+            string value = Unquote(split[1].Trim());
+
+            result[key] = value;
+        }
+
+        return result;
+    }
+
+    private static string Unquote(string s)
+    {
+        if (s.StartsWith("\"") && s.EndsWith("\""))
+            return s.Substring(1, s.Length - 2).Replace("\\\"", "\"");
+        return s;
     }
 }
